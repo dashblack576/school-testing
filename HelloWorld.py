eab3e15d6a7952a7a86fce57fab6cf4a892fa7a5
dashblack576 <59393700+dashblack576@users.noreply.github.com>
2022-08-29 08:23:08 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,16 @@
-print("hello world")
+def dayOne():
+
+  f = open('dayOne.txt','r')
+  puzzle_input = f.read()
+  
+  data = puzzle_input.split("\n")
+  data = [int(i) for i in data]  
+
+
+  print(data)
+  
+  for num in data:
+    for num_two in data:
+      if 2020 - num - num_two in data:
+        print(num * (2020 -num - num_two) * num_two)
+        return "" 
